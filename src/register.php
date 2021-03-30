ca028eb98d7b69095f5ce0cd7970d0f36714259e
adiscepo <attilio.discepoli@ulb.be>
2021-03-30 22:07:08 +0200

    Register fonctionne

--- src/register.php
+++ src/register.php
@@ -1,41 +1,50 @@
 <?php
     session_start();
-
+    
     function register($first_name, $last_name, $username, $password, $address, $isEpidemiologist, $center, $phone){
         $bdd = new PDO('mysql:host=127.0.0.1;dbname=Puff', 'puff-user', 'salade2fruit');
         
-        $username   = htmlspecialchars($username);
-        $password   = sha1($password);
-        $last_name  = htmlspecialchars($last_name);
-        $first_name = htmlspecialchars($first_name);
-        $address    = htmlspecialchars($address);
-        $center     = htmlspecialchars($center);
-        $phone      = htmlspecialchars($phone);
-        
         if (isset($username) && isset($password) && isset($last_name) && isset($first_name) && isset($address)) {
+            
+            $is_a_epidemiologist = 0;
+            if ($isEpidemiologist == "Y"){
+                if (!empty($center) && !empty($phone)) {
+                    $is_a_epidemiologist = 1;
+                    $center     = htmlspecialchars($center);
+                    $phone      = htmlspecialchars($phone);
+                }else{
+                    return "All fields must me complete for epidemiologist";
+                }
+            }    
+            
+            $first_name = htmlspecialchars($first_name);
+            $last_name  = htmlspecialchars($last_name);
+            $username   = htmlspecialchars($username);
+            $password   = sha1($password);
+            $address    = htmlspecialchars($address);
+            
             $req_username = $bdd->prepare('SELECT * FROM User WHERE username = ? LIMIT 1');
             $req_username->execute(array($username));
             $user_exist = $req_username->rowCount();
+            
             if ($user_exist == 0) {
                 $req_register = $bdd->prepare("INSERT INTO User(last_name, first_name, username, password, address, isEpidemiologist) VALUES(?, ?, ?, ?, ?, ?)");
-                $status = $req_register->execute(array($last_name, $first_name, $username, $password, $address, isset($isEpidemiologist)));
-                if(!empty($isEpidemiologist)){
-                    if (isset($center) && isset($phone)) {
-                        # Get id of user created
-                        $req_id_user = $bdd->prepare("SELECT ID FROM User WHERE username = ?");
-                        $req_id_user->execute(array($username));
-                        $id = $req_id_user->fetch();
-                        error_log("ID:", $id);
-                        # Insert the epidemiologist in db
-                        $req_register_epi = $bdd->prepare("INSERT INTO Epidemiologist(ID_USER, phone, centre) VALUES(?, ?, ?)");
-                        $status_epi = $req_register_epi->execute(array($id[0], $center, $phone));
-                        if ($status_epi) error_log("Epidemiologiste ajouté");
-                    }else{
-                        $res = "All fields must me complete for epidemiologist";
+                $status = $req_register->execute(array($last_name, $first_name, $username, $password, $address, $is_a_epidemiologist));
+                if ($status) error_log("la requete a fonctionné");
+                if($is_a_epidemiologist){
+                    # Get id of user created
+                    $req_id_user = $bdd->prepare("SELECT ID FROM User WHERE username = ?");
+                    $req_id_user->execute(array($username));
+                    $id = $req_id_user->fetch();
+                    error_log("ID:", $id);
+                    # Insert the epidemiologist in db
+                    $req_register_epi = $bdd->prepare("INSERT INTO Epidemiologist(ID_USER, phone, centre) VALUES(?, ?, ?)");
+                    $status_epi = $req_register_epi->execute(array($id[0], $center, $phone));
+                    if ($status_epi) error_log("Epidemiologiste ajouté");
+                    if ($status){ # l'insertion à fonctionné
+                        $res = "Registered !";
                     }
-                }
-                if ($status){ # l'insertion à fonctionné
-                    $res = "Ok t'es inscrit";
+                    error_log($res);
                 }
             }else{
                 $res= "Username already exist";
@@ -45,9 +54,9 @@
         }
         return $res;
     }
-
+        
     if (isset($_POST['submit_register'])) {
         $res = register($_POST["firstname"], $_POST["name"], $_POST["username"], $_POST["password"], $_POST["address"], $_POST["isEpidemiologist"], $_POST["center"], $_POST["phone"]);
         header('Location: ../index.html?' . $res . '#login');
     }
-?>
+    ?>
