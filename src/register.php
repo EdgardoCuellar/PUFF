f53726dc684ed20893ec958a5beb7b7d23769710
Edgardo Cuellar Sanchez <45764390+EdgardoCuellar@users.noreply.github.com>
2021-05-13 00:02:29 +0200

    AHHHHHHHHHHHHHHH
    NIK TA MERE REGISTER

--- src/register.php
+++ src/register.php
@@ -23,31 +23,30 @@
             $password   = sha1($password);
             $address    = htmlspecialchars($address);
             
-            $req_username = $bdd->prepare('SELECT * FROM User WHERE username = ? LIMIT 1');
+            $req_username = $bdd->prepare('SELECT DISTINCT * FROM User WHERE username = ?');
             $req_username->execute(array($username));
             $user_exist = $req_username->rowCount();
             
             if ($user_exist == 0) {
-                $req_register = $bdd->prepare("INSERT INTO User(last_name, first_name, username, password, address, isEpidemiologist) VALUES(?, ?, ?, ?, ?, ?)");
-                $status = $req_register->execute(array($last_name, $first_name, $username, $password, $address, $is_a_epidemiologist ? 0x1 : 0x0));
+                $status = $bdd->exec("INSERT INTO User(last_name, first_name, username, password, address, isEpidemiologist) VALUES('".$last_name."', '".$first_name."', '".$username."', '".$password."', '".$address."', " . $is_a_epidemiologist . ")");
                 if ($status) {
-                     $res = "ok";
-                     error_log("la requete a fonctionné");
-                } 
-                if($is_a_epidemiologist){
+                    $res = "ok";
                     # Get id of user created
                     $req_id_user = $bdd->prepare("SELECT ID FROM User WHERE username = ?");
                     $req_id_user->execute(array($username));
                     $id = $req_id_user->fetch();
-                    error_log("ID:", $id);
-                    # Insert the epidemiologist in db
-                    $req_register_epi = $bdd->prepare("INSERT INTO Epidemiologist(ID_USER, phone, centre) VALUES(?, ?, ?)");
-                    $status_epi = $req_register_epi->execute(array($id[0], $center, $phone));
-                    if ($status_epi) error_log("Epidemiologiste ajouté");
-                    if ($status){ # l'insertion à fonctionné
-                        $res = "ok";
+                    $_SESSION['id'] = intval($id[0]);
+                    error_log("la requete a fonctionné");
+                    if($is_a_epidemiologist){
+                        # Insert the epidemiologist in db
+                        $req_register_epi = $bdd->prepare("INSERT INTO Epidemiologist(ID_USER, phone, centre) VALUES(?, ?, ?)");
+                        $status_epi = $req_register_epi->execute(array($id[0], $center, $phone));
+                        if ($status_epi) error_log("Epidemiologiste ajouté");
+                        if ($status){ # l'insertion à fonctionné
+                            $res = "ok";
+                        }
+                        error_log($res);
                     }
-                    error_log($res);
                 }
             }else{
                 $res= "Username already exist";
@@ -61,7 +60,7 @@
     if (isset($_POST['submit_register'])) {
         $res = register($_POST["firstname"], $_POST["name"], $_POST["username"], $_POST["password"], $_POST["address"], $_POST["isEpidemiologist"], $_POST["center"], $_POST["phone"]);
         if ($res=="ok") {
-            header('Location: ../db_main.php#');
+            header('Location: ../db_main.php');
         } else { 
             error_log($res);
             header('Location: ../index.php?error_msg='.$res.'#register');
